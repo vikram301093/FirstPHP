c41ab358954c228b9333678492b12d17f8552f5f
vikram301093 <vikram.singhkushwaha@atos.net>
2017-05-29 17:26:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head><title>Random</title></head>
 <body>
-<p>I have randomly selected the number <?php
+<p>I have randomly selected the number is the<?php
 $choice = rand(1, 100);
 echo $choice;
 ?>. Its square root is <?php
